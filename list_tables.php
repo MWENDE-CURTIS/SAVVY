1198325c79ceb3b64e9231de10c61374f14ae83c
MWENDE-CURTIS <mwende.curtis@gmail.com>
2025-11-30 05:44:07 +0300

    list tables on remote db

---
+++ list_tables.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/connect.php';
+if (!$conn) { die("No DB connection\n"); }
+$res = $conn->query("SHOW TABLES");
+while ($row = $res->fetch_row()) {
+    echo $row[0] . "\n";
+}
+?>
